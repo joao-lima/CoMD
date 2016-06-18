4da8edb9ff70299aba33ca516633b2a9b84fd9d4
648trindade <rafaelgt.mbin@gmail.com>
2016-06-18 18:30:56 -0300

    Kokkos added

---
+++ src-kokkos/CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "cc"
+#define CoMD_HOSTNAME "dell-a30"
+#define CoMD_KERNEL_NAME "'Linux'"
+#define CoMD_KERNEL_RELEASE "'4.5.0-2-amd64'"
+#define CoMD_PROCESSOR "'unknown'"
+
+#define CoMD_COMPILER "''"
+#define CoMD_COMPILER_VERSION "'unknown'"
+#define CoMD_CFLAGS "''"
+#define CoMD_LDFLAGS "''"
+
+#endif
